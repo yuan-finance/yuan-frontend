b4db8fb7bcffba044ba49636a1f387a181a854b6
chief <chief@1yuan.finance>
2020-12-14 11:51:55 +0800

    update new treasury contract

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -7,5 +7,5 @@
 export const yam = '0x4A3e164684812DfB684AC36457E7fA805087c68E'
 export const usdx_adress = '0xeb269732ab75a6fd61ea60b06fe994cd32a83549'
 export const oracle_address = '0x8ABb6cc2810c22Df468E5fC491AB693F0901c927'
-export const Treasury = '0x7ba4e109c1dc8B52ed63D8EdF0e951685DDe4DA6'
+export const Treasury = '0xB37C599fbDD3f1C30fcAe51194ec802E52f70f61'
 
