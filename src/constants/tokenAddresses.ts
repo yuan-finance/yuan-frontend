4842d44fb6b72b7326c912c1e27f713a30727961
yuan-finance <72286648+yuan-finance@users.noreply.github.com>
2021-03-24 17:07:27 +0800

    update oracle contract.

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -6,6 +6,6 @@
 // main net
 export const yam = '0x4A3e164684812DfB684AC36457E7fA805087c68E'
 export const usdx_adress = '0xeb269732ab75a6fd61ea60b06fe994cd32a83549'
-export const oracle_address = '0x8ABb6cc2810c22Df468E5fC491AB693F0901c927'
+export const oracle_address = '0x95122089705594960a7c2074206d01679b073e69'
 export const Treasury = '0xB37C599fbDD3f1C30fcAe51194ec802E52f70f61'
 
